f1901294012c7517e703aca4ea33d4088bf5d6a2
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 14:58:31 -0700

    Add a webview widget

--- app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/RecipeDetailActivity.java
+++ app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/RecipeDetailActivity.java
@@ -2,8 +2,10 @@ package com.atempa.mvp.dam2018_02_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class RecipeDetailActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
