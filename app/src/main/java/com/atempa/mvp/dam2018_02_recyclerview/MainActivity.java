549a726829faa9c96e477a4c893291ab4f4e0858
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 13:51:25 -0700

    Initial structure of Android project

---
+++ app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.atempa.mvp.dam2018_02_recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
