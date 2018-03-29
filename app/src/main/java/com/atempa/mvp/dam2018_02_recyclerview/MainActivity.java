a29efbc92395aa7815125c8458aa942136960966
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 15:02:01 -0700

    Add click event for adapter items

--- app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/MainActivity.java
+++ app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/MainActivity.java
@@ -1,8 +1,11 @@
 package com.atempa.mvp.dam2018_02_recyclerview;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
 
         RecipeAdapter adapter = new RecipeAdapter(this, recipeList);
         mListView.setAdapter(adapter);
+
+        final Context context = this;
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 1
+                Recipe selectedRecipe = recipeList.get(position);
+                // 2
+                Intent detailIntent = new Intent(context, RecipeDetailActivity.class);
+                // 3
+                detailIntent.putExtra("title", selectedRecipe.title);
+                detailIntent.putExtra("url", selectedRecipe.instructionUrl);
+                // 4
+                startActivity(detailIntent);
+            }
+        });
     }
 }
