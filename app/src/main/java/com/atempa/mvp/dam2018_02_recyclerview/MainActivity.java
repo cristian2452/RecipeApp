dc6b44f55e67831f90fc4246dd24d19426acfd34
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 14:10:07 -0700

    Populate recycler view with dataset of recipes

--- app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/MainActivity.java
+++ app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/MainActivity.java
@@ -2,12 +2,31 @@ package com.atempa.mvp.dam2018_02_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.recipe_list_view);
+
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+
+        String[] listItems = new String[recipeList.size()];
+
+        for(int i = 0; i < recipeList.size(); i++){
+            Recipe recipe = recipeList.get(i);
+            listItems[i] = recipe.title;
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
     }
 }
