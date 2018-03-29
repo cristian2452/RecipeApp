3b3dfe7c3feba64ea9204c1c7eefd38ce90e6c98
Jorge Atempa <atempaitm@gmail.com>
2018-03-29 14:56:27 -0700

    Create a new empty activity

---
+++ app/src/main/java/com/atempa/mvp/dam2018_02_recyclerview/RecipeDetailActivity.java
@@ -0,0 +1,13 @@
+package com.atempa.mvp.dam2018_02_recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
